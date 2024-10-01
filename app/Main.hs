f7cd200ae2deea52f0ed6218a256117819c96742
Pujo <pujoheadsoft@gmail.com>
2024-10-02 06:31:59 +0900

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
 module Main where
+import Data.Effect.TH (makeEffectF)
+import Data.Hefty.Extensible (type (<|), ForallHFunctor)
+import Control.Effect (type (~>))
+import Control.Effect.ExtensibleFinal (type (:!!))
+import Control.Effect.Hefty (interpretRec)
+import Control.Monad.IO.Class (liftIO)
+
+data Teletype a where
+  ReadTTY :: Teletype String
+  WriteTTY :: String -> Teletype ()
+
+makeEffectF [''Teletype]
+
+teletypeToIO :: (IO <| r, ForallHFunctor eh) => eh :!! LTeletype ': r ~> eh :!! r
+teletypeToIO = interpretRec \case
+    ReadTTY -> liftIO getLine
+    WriteTTY msg -> liftIO $ putStrLn msg
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
