2db64d00267d9d96fcb53c6ba6095bb1d05f7c72
Pujo <pujoheadsoft@gmail.com>
2024-10-03 07:15:45 +0900

    wip

--- app/Effects/HigherOrderEffect.hs
+++ app/Effects/HigherOrderEffect.hs
@@ -7,8 +7,18 @@ import Control.Effect.ExtensibleFinal (type (:!!))
 import Control.Effect.Hefty (interpretRec, runEff, interposeRec)
 import Control.Monad.IO.Class (liftIO)
 import Data.Text (Text)
+import Data.Text.IO qualified as T
+import Data.Time (UTCTime)
 
 data Log a where
   Logging :: Text -> Log ()
 
 makeEffectF [''Log]
+
+logToIO :: (IO <| r, ForallHFunctor eh) => eh :!! LLog ': r ~> eh :!! r
+logToIO = interpretRec \(Logging msg) -> liftIO $ T.putStrLn msg
+
+data Time a where
+  CurrentTime :: Time UTCTime
+
+makeEffectF [''Time]
