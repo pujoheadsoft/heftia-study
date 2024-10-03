a0983f871874bc7b19a4fe579bc6e97d50d271fb
Pujo <pujoheadsoft@gmail.com>
2024-10-04 07:13:58 +0900

    wip

--- app/Effects/HigherOrderEffect.hs
+++ app/Effects/HigherOrderEffect.hs
@@ -1,16 +1,19 @@
 module Effects.HigherOrderEffect where
 
 import Data.Effect.TH (makeEffectF, makeEffectH)
-import Data.Hefty.Extensible (type (<|), ForallHFunctor)
+import Data.Hefty.Extensible (type (<|), ForallHFunctor, type (<<|))
 import Control.Effect (type (~>), type (<:), type (<<:))
 import Control.Effect.ExtensibleFinal (type (:!!))
-import Control.Effect.Hefty (interpretRec, runEff, interposeRec, interpretRecH)
+import Control.Effect.Hefty (interpretRec, runEff, interposeRec, interpretRecH, raise, raiseH, interposeRecH)
 import Control.Monad.IO.Class (liftIO, MonadIO)
-import Data.Text (Text, pack)
+import Data.Text (Text, pack, unpack)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Time (UTCTime, formatTime, defaultTimeLocale, getCurrentTime)
 import Data.Kind (Type)
+import Control.Effect.Interpreter.Heftia.Reader (runReader)
+import Control.Category ((>>>))
+import Data.Effect.Reader (Local, LAsk, local, ask)
 
 data Log a where
   Logging :: Text -> Log ()
@@ -73,3 +76,32 @@ logExample = do
   logging $ pack "out of chunk scope1 3"
   logging $ pack "out of chunk scope1 4"
 --------------------------------------------
+data FileSystem a where
+  Mkdir :: FilePath -> FileSystem ()
+  WriteToFile :: FilePath -> String -> FileSystem ()
+
+makeEffectF [''FileSystem]
+
+runDymmyFS :: (IO <| r, ForallHFunctor eh) => eh :!! LFileSystem ': r ~> eh :!! r
+runDymmyFS = interpretRec \case
+    Mkdir path -> liftIO $ putStrLn $ "<runDummyFS> mkdir " <> path
+    WriteToFile path content -> liftIO $ putStrLn $ "<runDummyFS> writeToTile" <> path <> " : " <> content
+-----------------------------------
+saveLogChunk ::
+  (LogChunk <<| eh, Log <| ef, FileSystem <| eh, Time <| ef, ForallHFunctor eh) =>
+  eh :!! ef ~> eh :!! ef
+saveLogChunk = raise
+  >>> raiseH
+  >>> hookCreateDirectory
+  >>> hookWriteFile
+  >>> runReader @FilePath "./log/"
+
+hookCreateDirectory :: (Local FilePath ': eh :!! LAsk FilePath ': ef) ~> (Local FilePath ': eh :!! LAsk FilePath ': ef)
+hookCreateDirectory =
+  interposeRecH \(LogChunk chunkName a) -> logChunk chunkName do
+    chungBegingAt <- currentTime
+    let dirName = unpack $ iso8601 chungBegingAt <> pack "-" <> chunkName
+    local @FilePath (++ dirName ++ "/") do
+      logChunkPath <- ask
+      mkdir logChunkPath
+      a
