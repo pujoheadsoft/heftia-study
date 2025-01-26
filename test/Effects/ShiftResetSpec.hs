959fa8c7d7132e1cbcb6c159bfbf1a7e918ab846
Pujo <pujoheadsoft@gmail.com>
2025-01-26 19:22:46 +0900

    wip

--- test/Effects/ShiftResetSpec.hs
+++ test/Effects/ShiftResetSpec.hs
@@ -11,7 +11,7 @@ module Effects.ShiftResetSpec where
 import Prelude hiding (either, any)
 import Test.Hspec
 
-import Data.Effect.ShiftReset ( reset, shiftF, shift, Reset, ShiftF, Shift_, embedF, ShiftKey, Shift' )
+import Data.Effect.ShiftReset ( reset, shiftF, shift, Reset, ShiftF, Shift_, embedF, ShiftKey, Shift', exit )
 import Control.Monad.Hefty
 
 import Control.Monad.Hefty.ShiftReset ( runReset, runShiftF, Shift, exitF, evalShift )
@@ -21,57 +21,31 @@ import Data.Text (pack)
 import Control.Effect.Key (SendHOEBy)
 
 {-
-data Reset m (a :: Type) where
-    Reset :: m a -> Reset m a
-makeEffectH [''Reset]
+  data Reset m (a :: Type) where
+      Reset :: m a -> Reset m a
+  makeEffectH [''Reset]
 
-data Shift' (ans :: Type) n m a where
-    Shift
-        :: forall ans n m a
-         . ((a -> n ans) -> (forall x. m x -> n x) -> n ans)
-        -> Shift' ans n m a
-makeKeyedEffect [] [''Shift']
+  data Shift' (ans :: Type) n m a where
+      Shift
+          :: forall ans n m a
+          . ((a -> n ans) -> (forall x. m x -> n x) -> n ans)
+          -> Shift' ans n m a
+  makeKeyedEffect [] [''Shift']
 
-data ShiftF ans a where
-    ShiftF :: forall ans a. ((a -> ans) -> ans) -> ShiftF ans a
-
-ShiftF (Eff '[] '[IO] String) だとしたら
-ansが (Eff '[] '[IO] String) だから
-
-shiftF :: ((a -> (Eff '[] '[IO] String)) -> (Eff '[] '[IO] String)) -> ShiftF (Eff '[] '[IO] String) a
-となるわけか。
-したがって shiftF k の k は (a -> (Eff '[] '[IO] String)) という型だな。
-
-Eff は Eff eh ef a という定義だ。つまり高階はなく、一階のエフェクトとしてIOがあるということだ。扱うのはString。
-
--}
-
--- r :: (Reset <<: m, Shift_ (Eff '[Reset] '[] String) <: m, Monad m) => m String
--- r = reset $ shiftF \k -> k $ pure "x"
-{-
-  ShiftFの型はShiftF answertype a で
-  runShiftFはこういう定義になっている。
-  runShiftF :: Eff '[] (ShiftF (Eff '[] ef ans) ': ef) ans -> Eff '[] ef ans
-  したがってShiftFはShiftF (Eff '[] ef ans) ': efという型でないといけない。
-  今回efはなくていいから ShiftF (Eff '[] '[] String) という型になる。
-  そして、ShiftFの定義に上を当てはめると
   data ShiftF ans a where
       ShiftF :: forall ans a. ((a -> ans) -> ans) -> ShiftF ans a
-  が
-  data ShiftF (Eff '[] '[] String) a where
-    ShiftF :: ((a -> (Eff '[] '[] String)) -> Eff '[] '[] String) -> ShiftF (Eff '[] '[] String) a
 
-
+  evalShift :: Eff '[Shift ans '[] ef] ef ans -> Eff '[] ef ans
+  runShift  :: (a -> Eff '[] ef ans) -> Eff '[Shift ans '[] ef] ef a -> Eff '[] ef ans
+  runShift_ :: Eff (Shift_ (Eff eh ef) ': eh) ef ~> Eff eh ef
+  runShiftF :: Eff '[] (ShiftF (Eff '[] ef ans) ': ef) ans -> Eff '[] ef ans
+  runReset  :: Eff (Reset ': eh) ef ~> Eff eh ef
+  runEff    :: Monad m => Eff '[] '[m] ~> m
 -}
--- xx :: (Reset <<: m, ShiftF (Eff '[] '[] String) <: m, Monad m) => m String
-xx :: (Reset <<: m, SendHOEBy ShiftKey (Shift' String n) m, Monad m) => m String
-xx = reset do
-  x <- shift \k _ -> k "x"
-  pure $ x <> "y"
 
 spec :: Spec
 spec = do
-  describe "とりあえず動かす" do
+  describe "限定継続のテスト" do
     it "reset" do
       let 
         r :: (Reset <<: m, Monad m) => m String
@@ -79,69 +53,35 @@ spec = do
       x <- (runEff <<< runReset) r
       x `shouldBe` "x"
 
-    it "reset" do
+    it "継続を使って計算することができる" do
       let 
-        r :: (Reset <<: m, ShiftF (Eff '[] '[IO] String) <: m, Monad m) => m String
-        r = reset do
-          --x <- shiftF \k -> k $ pure "x"
-          pure ""
-      -- evalShift :: Eff '[Shift ans '[] ef] ef ans -> Eff '[] ef ans
-      -- runShift  :: (a -> Eff '[] ef ans) -> Eff '[Shift ans '[] ef] ef a -> Eff '[] ef ans
-      -- runShift_ :: Eff (Shift_ (Eff eh ef) ': eh) ef ~> Eff eh ef
-      -- runShiftF :: Eff '[] (ShiftF (Eff '[] ef ans) ': ef) ans -> Eff '[] ef ans
-      -- runReset  :: Eff (Reset ': eh) ef ~> Eff eh ef
-      -- runEff    :: Monad m => Eff '[] '[m] ~> m
-      x <- (runEff <<< evalShift <<< runReset) xx
+        program :: (Reset <<: m, SendHOEBy ShiftKey (Shift' String n) m, Monad m) => m String
+        program = reset do
+          x <- shift \k _ -> k "x"
+          pure $ x <> "y"
+
+      x <- (runEff <<< evalShift <<< runReset) program
       x `shouldBe` "xy"
 
-  --describe "基本的なオペレーターのテスト" do
-    -- it "pushPrompt" do
-    --   runCC (newPrompt >>= \p -> pushPrompt p (pure "x")) `shouldBe` "x"
+    it "exitで継続を破棄することができる" do
+      let 
+        program :: (Reset <<: m, SendHOEBy ShiftKey (Shift' Int n) m, Monad m, Applicative n) => m Int
+        program = reset do
+          x <- exit $ 5 * 2
+          pure $ 3 + x - 1 -- 継続部分 3 + [..] - 1は破棄される
 
-    -- it "reset" do
-    --   runCC (reset \_ -> pure "x") `shouldBe` "x"
+      r <- (runEff <<< evalShift <<< runReset) program
+      r `shouldBe` 10
 
-    -- it "withSubCont" do
-    --   runCC (reset \p -> (1:) <$> (2:) <$> withSubCont p (\_ -> return [])) `shouldBe` []
+    it "継続を使って実行順序を変更できる" do
+      let
+        program :: (Reset <<: m, SendHOEBy ShiftKey (Shift' Int n) m, Monad m, Functor n) => m Int
+        program = reset do
+          s <- shift \k _ -> (* 2) <$> k 3
+          pure $ 1 + s -- 継続部分 3 + [..] - 1は破棄される
 
-    -- it "pushSubCont" do
-    --   runCC (reset \p -> (1:) <$> (2:) <$> withSubCont p (\k -> pushSubCont k (return []))) `shouldBe` [1, 2]
-
-  describe "限定継続のテスト" do
-    it "継続を使って計算することができる" do
-   
-      "1" `shouldBe` "1"
-
-  --   it "継続を破棄することができる(shift版)" do
-  --     let r = runCC $ reset $ \p -> do
-  --           s <- shift p $ \_ -> pure $ 5 * 2
-  --           pure $ 3 + s - 1 -- 継続部分 3 + [..] - 1は破棄される
-  --     r `shouldBe` 10
-
-  --   it "pushPromptで計算を区切った中のshiftで継続を破棄しても外側の継続は破棄されない" do
-  --     let r = runCC $ reset $ \p -> do
-  --           s <- pushPrompt p $ shift p $ \_ -> pure $ 5 * 2
-  --           pure $ 3 + s - 1 -- 継続部分 3 + [..] - 1は破棄されない
-  --     r `shouldBe` 12
-
-  --   it "pushPromptで計算を区切った中のshiftで継続を破棄しても外側の継続は破棄されないが、内側の継続は破棄される" do
-  --     let r = runCC $ reset $ \p -> do
-  --           -- 継続 [..] * 10 は破棄される
-  --           s <- pushPrompt p ((* 10) <$> shift p (\_ -> pure $ 5 * 2))
-  --           pure $ 3 + s - 1 -- 継続部分 3 + [..] - 1は破棄さない
-  --     r `shouldBe` 12
-
-  --   it "継続を破棄することができる(abort版)" do
-  --     let r = runCC $ reset $ \p -> do
-  --           s <- abort p $ pure $ 5 * 2
-  --           pure $ 3 + s - 1 -- 継続部分 3 + [..] - 1は破棄される
-  --     r `shouldBe` 10
-
-  --   it "継続を使って実行順序を変更できる" do
-  --     let r = runCC $ reset $ \p -> do
-  --           s <- shift p \k -> (* 2) <$> k 3
-  --           pure $ 1 + s
-  --     r `shouldBe` 8 -- 2 * 3 + 1 に見えるが、(1 + 3) * 2 になる
+      r <- (runEff <<< evalShift <<< runReset) program
+      r `shouldBe` 8 -- 2 * 3 + 1 に見えるが、(1 + 3) * 2 になる
 
   --   it "継続を複数回使うことができる" do
 
