ed6752c0d41370b429694ef03186e7d0ca64ce45
Pujo <pujoheadsoft@gmail.com>
2025-01-28 08:00:32 +0900

    wip

--- test/Effects/HigherOrderEffectSpec.hs
+++ test/Effects/HigherOrderEffectSpec.hs
@@ -20,33 +20,32 @@ toUTCTime timeStr = unsafePerformIO $ do
 spec :: Spec
 spec = do
   describe "高階なエフェクトフルプログラムのテスト" do
-    describe "logWithTime" do
-      it "ユーザー入力が一回あったら、ユーザー入力が一度出力される" do
-        readTTYStubFn <- createStubFn do 
-          onCase $ pure @IO (toUTCTime "2025-01-28 00:00:00")
-          onCase $ pure @IO (toUTCTime "2025-01-28 01:00:00")
-          onCase $ pure @IO (toUTCTime "2025-01-28 02:00:00")
-          
-
-        logMock <- createMock $ any @Text |> pure @IO ()
+    it "logWithTime 時刻つきでログが出力される" do
+      readTTYStubFn <- createStubFn do 
+        onCase $ pure @IO (toUTCTime "2025-01-28 00:00:00")
+        onCase $ pure @IO (toUTCTime "2025-01-28 01:00:00")
+        onCase $ pure @IO (toUTCTime "2025-01-28 02:00:00")
         
-        let 
-          logToIO :: (IO <| r) => eh :!! Log ': r ~> eh :!! r
-          logToIO = interpret \(Logging msg) -> liftIO $ stubFn logMock msg
 
-          timeToIO :: (IO <| r) => eh :!! Time ': r ~> eh :!! r
-          timeToIO = interpret \CurrentTime -> do
-            liftIO readTTYStubFn
+      logMock <- createMock $ any @Text |> pure @IO ()
+      
+      let 
+        logToIO :: (IO <| r) => eh :!! Log ': r ~> eh :!! r
+        logToIO = interpret \(Logging msg) -> liftIO $ stubFn logMock msg
 
-        r <- (runEff . logToIO . timeToIO . logWithTime) do
-          logging $ pack "foo"
-          logging $ pack "bar"
-          logging $ pack "baz"
-          
-        r `shouldBe` ()
-        logMock `shouldApplyInOrder` [
-            pack "[2025-01-28T00:00:00.000Z] foo"
-          , pack "[2025-01-28T01:00:00.000Z] bar"
-          , pack "[2025-01-28T02:00:00.000Z] baz" 
-          ]
+        timeToIO :: (IO <| r) => eh :!! Time ': r ~> eh :!! r
+        timeToIO = interpret \CurrentTime -> do
+          liftIO readTTYStubFn
+
+      r <- (runEff . logToIO . timeToIO . logWithTime) do
+        logging $ pack "foo"
+        logging $ pack "bar"
+        logging $ pack "baz"
+        
+      r `shouldBe` ()
+      logMock `shouldApplyInOrder` [
+          pack "[2025-01-28T00:00:00.000Z] foo"
+        , pack "[2025-01-28T01:00:00.000Z] bar"
+        , pack "[2025-01-28T02:00:00.000Z] baz" 
+        ]
 
