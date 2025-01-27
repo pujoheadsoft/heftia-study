e067a5487cba30d07e3cace3297f01c6c4365ca9
Pujo <pujoheadsoft@gmail.com>
2025-01-28 07:28:47 +0900

    wip

--- test/Effects/SimpleEffectSpec.hs
+++ test/Effects/SimpleEffectSpec.hs
@@ -12,36 +12,72 @@ import Prelude hiding (any)
 spec :: Spec
 spec = do
   describe "単純なエフェクトフルプログラムのテスト" do
-    it "echo - ユーザー入力が一回あったら、ユーザー入力が一度出力される" do
-      readTTYStubFn <- createStubFn do 
-        onCase $ pure @IO "something"
-        onCase $ pure @IO ""
+    describe "echo" do
+      it "ユーザー入力が一回あったら、ユーザー入力が一度出力される" do
+        readTTYStubFn <- createStubFn do 
+          onCase $ pure @IO "something"
+          onCase $ pure @IO ""
 
-      writeTTYMock <- createMock $ any @String |> pure @IO ()
-      
-      let 
-        run :: (IO <| r) => eh :!! Teletype ': r ~> eh :!! r
-        run = interpret $ \case
-          ReadTTY -> liftIO readTTYStubFn
-          WriteTTY msg -> liftIO $ stubFn writeTTYMock msg
-
-      r <- (runEff . run) echo
+        writeTTYMock <- createMock $ any @String |> pure @IO ()
         
-      r `shouldBe` ()
-      writeTTYMock `shouldApplyTo` "something"
+        let 
+          run :: (IO <| r) => eh :!! Teletype ': r ~> eh :!! r
+          run = interpret $ \case
+            ReadTTY -> liftIO readTTYStubFn
+            WriteTTY msg -> liftIO $ stubFn writeTTYMock msg
 
-    it "echo - ユーザー入力が一回もなかったら、出力はされない" do
-      readTTYStubFn <- createStubFn $ pure @IO ""
+        r <- (runEff . run) echo
+          
+        r `shouldBe` ()
+        writeTTYMock `shouldApplyTo` "something"
 
-      writeTTYMock <- createMock $ any @String |> pure @IO ()
-      
-      let 
-        run :: (IO <| r) => eh :!! Teletype ': r ~> eh :!! r
-        run = interpret $ \case
-          ReadTTY -> liftIO readTTYStubFn
-          WriteTTY msg -> liftIO $ stubFn writeTTYMock msg
+      it "ユーザー入力が一回もなかったら、出力はされない" do
+        readTTYStubFn <- createStubFn $ pure @IO ""
 
-      r <- (runEff . run) echo
+        writeTTYMock <- createMock $ any @String |> pure @IO ()
         
-      r `shouldBe` ()
-      writeTTYMock `shouldApplyTimesToAnything` (0 :: Int)
+        let 
+          run :: (IO <| r) => eh :!! Teletype ': r ~> eh :!! r
+          run = interpret $ \case
+            ReadTTY -> liftIO readTTYStubFn
+            WriteTTY msg -> liftIO $ stubFn writeTTYMock msg
+
+        r <- (runEff . run) echo
+          
+        r `shouldBe` ()
+        writeTTYMock `shouldApplyTimesToAnything` (0 :: Int)
+
+    describe "strong" do
+      it "ユーザー入力が一回あったら、出力の際ユーザー入力に!が付与される" do
+        readTTYStubFn <- createStubFn do 
+          onCase $ pure @IO "something"
+          onCase $ pure @IO ""
+
+        writeTTYMock <- createMock $ any @String |> pure @IO ()
+        
+        let 
+          run :: (IO <| r) => eh :!! Teletype ': r ~> eh :!! r
+          run = interpret $ \case
+            ReadTTY -> liftIO readTTYStubFn
+            WriteTTY msg -> liftIO $ stubFn writeTTYMock msg
+
+        r <- (runEff . run. strong . strong) echo
+          
+        r `shouldBe` ()
+        writeTTYMock `shouldApplyTo` "something!!"
+
+      it "ユーザー入力が一回もなかったら、出力はされない" do
+        readTTYStubFn <- createStubFn $ pure @IO ""
+
+        writeTTYMock <- createMock $ any @String |> pure @IO ()
+        
+        let 
+          run :: (IO <| r) => eh :!! Teletype ': r ~> eh :!! r
+          run = interpret $ \case
+            ReadTTY -> liftIO readTTYStubFn
+            WriteTTY msg -> liftIO $ stubFn writeTTYMock msg
+
+        r <- (runEff . run . strong . strong) echo
+          
+        r `shouldBe` ()
+        writeTTYMock `shouldApplyTimesToAnything` (0 :: Int)
