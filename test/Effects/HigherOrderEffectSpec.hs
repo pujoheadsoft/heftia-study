e03aae78ef1726a4b17fc2213f1abac893d15d72
Pujo <pujoheadsoft@gmail.com>
2025-01-28 07:49:00 +0900

    wip

--- test/Effects/HigherOrderEffectSpec.hs
+++ test/Effects/HigherOrderEffectSpec.hs
@@ -10,21 +10,33 @@ import Test.MockCat
 import Prelude hiding (any)
 import Effects.HigherOrderEffect
 import Data.Text (Text, pack, unpack)
+import Data.Time
+import System.IO.Unsafe
 
+toUTCTime :: String -> UTCTime
+toUTCTime timeStr = unsafePerformIO $ do
+  return $ parseTimeOrError True defaultTimeLocale "%Y-%m-%d %H:%M:%S" timeStr
+  
 spec :: Spec
 spec = do
   describe "高階なエフェクトフルプログラムのテスト" do
     describe "logWithTime" do
       it "ユーザー入力が一回あったら、ユーザー入力が一度出力される" do
-        writeTTYMock <- createMock $ any @Text |> pure @IO ()
+        readTTYStubFn <- createStubFn do 
+          onCase $ pure @IO (toUTCTime "2025-01-28 00:00:00")
+          onCase $ pure @IO (toUTCTime "2025-01-28 01:00:00")
+          onCase $ pure @IO (toUTCTime "2025-01-28 02:00:00")
+          
+
+        logMock <- createMock $ any @Text |> pure @IO ()
         
         let 
           logToIO :: (IO <| r) => eh :!! Log ': r ~> eh :!! r
-          logToIO = interpret \(Logging msg) -> liftIO $ stubFn writeTTYMock msg
+          logToIO = interpret \(Logging msg) -> liftIO $ stubFn logMock msg
 
           timeToIO :: (IO <| r) => eh :!! Time ': r ~> eh :!! r
           timeToIO = interpret \CurrentTime -> do
-            liftIO $ pure @IO (read "2025-01-28 00:00:00 UTC")
+            liftIO readTTYStubFn
 
         r <- (runEff . logToIO . timeToIO . logWithTime) do
           logging $ pack "foo"
@@ -32,9 +44,9 @@ spec = do
           logging $ pack "baz"
           
         r `shouldBe` ()
-        writeTTYMock `shouldApplyInOrder` [
+        logMock `shouldApplyInOrder` [
             pack "[2025-01-28T00:00:00.000Z] foo"
-          , pack "[2025-01-28T00:00:00.000Z] bar"
-          , pack "[2025-01-28T00:00:00.000Z] baz" 
+          , pack "[2025-01-28T01:00:00.000Z] bar"
+          , pack "[2025-01-28T02:00:00.000Z] baz" 
           ]
 
