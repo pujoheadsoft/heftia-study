8d8d3438164c1b858c9719bf632cd08aea65044b
Pujo <pujoheadsoft@gmail.com>
2025-01-28 07:25:24 +0900

    wip

--- test/Effects/SimpleEffectSpec.hs
+++ test/Effects/SimpleEffectSpec.hs
@@ -7,31 +7,41 @@ import Test.Hspec
 import Control.Monad.Hefty
 import Effects.SimpleEffect
 import Test.MockCat
-import Control.Monad.Hefty.State (put)
+import Prelude hiding (any)
 
 spec :: Spec
 spec = do
   describe "単純なエフェクトフルプログラムのテスト" do
-    it "echo" do
+    it "echo - ユーザー入力が一回あったら、ユーザー入力が一度出力される" do
       readTTYStubFn <- createStubFn do 
-        onCase $ pure @IO "output"
+        onCase $ pure @IO "something"
         onCase $ pure @IO ""
 
-      writeTTYMock <- createMock $ "output" |> ()
+      writeTTYMock <- createMock $ any @String |> pure @IO ()
       
       let 
         run :: (IO <| r) => eh :!! Teletype ': r ~> eh :!! r
         run = interpret $ \case
-          ReadTTY -> liftIO do
-            value <- readTTYStubFn
-            putStrLn $ "ReadTTY:" <> value
-            pure value
-          WriteTTY msg -> do
-            liftIO $ putStrLn $ "呼ばれたよ:"<> msg
-            pure $ stubFn writeTTYMock msg
+          ReadTTY -> liftIO readTTYStubFn
+          WriteTTY msg -> liftIO $ stubFn writeTTYMock msg
 
       r <- (runEff . run) echo
         
       r `shouldBe` ()
+      writeTTYMock `shouldApplyTo` "something"
 
-      --writeTTYMock `shouldApplyTo` "output"
+    it "echo - ユーザー入力が一回もなかったら、出力はされない" do
+      readTTYStubFn <- createStubFn $ pure @IO ""
+
+      writeTTYMock <- createMock $ any @String |> pure @IO ()
+      
+      let 
+        run :: (IO <| r) => eh :!! Teletype ': r ~> eh :!! r
+        run = interpret $ \case
+          ReadTTY -> liftIO readTTYStubFn
+          WriteTTY msg -> liftIO $ stubFn writeTTYMock msg
+
+      r <- (runEff . run) echo
+        
+      r `shouldBe` ()
+      writeTTYMock `shouldApplyTimesToAnything` (0 :: Int)
