4e500c7e12ca979fc370be5f761b83fa152775c6
Pujo <pujoheadsoft@gmail.com>
2025-01-29 07:44:09 +0900

    wip

---
+++ src/Effects/Experiment.hs
@@ -0,0 +1,18 @@
+-- 色々試してみるためのモジュール
+module Effects.Experiment where
+
+import Control.Monad.Hefty
+
+data X a where
+  X :: X String
+
+makeEffectF [''X]
+
+data Y a where
+  Y :: Y Int
+
+makeEffectF [''Y]
+
+t :: eh :!! X ': ef ~> eh :!! Y ': ef
+t = transform \case
+  X -> undefined
