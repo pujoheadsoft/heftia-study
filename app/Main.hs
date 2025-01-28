b61d7b35ac8fae75e38a0b6c20b79d28708b6504
Pujo <pujoheadsoft@gmail.com>
2025-01-29 07:21:44 +0900

    Heftiaに関係ないやつらを移した

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,13 @@
 module Main (main) where
-
-import DelimitedContinuation.BuiltinCC as DCE1
-import DelimitedContinuation.Example2 as DCE2
-import DelimitedContinuation.CCDelcont.Example as CCDelcontExample
-import DelimitedContinuation.CCDelcont.TreeIterator as CCDelcontTreeExample
-import DelimitedContinuation.CCDelcont.BreadthFirstTraversal as CCDelcontBreadthFirstTraversal
-import DelimitedContinuation.CCDelcont.ResumableParsing as CCDelcontResumableParsing
-
 import Effects.SimpleEffect as Simple
 import Effects.HigherOrderEffect as HigherOrder
 import Effects.ResetFork as ResetFork
 
 main :: IO ()
 main = do
-  -- Simple.program
-  -- HigherOrder.program
-  -- HigherOrder.program2
-  -- HigherOrder.program3
-  -- ResetFork.program
-  DCE1.program
-  -- DCE2.program
-  -- DCE2.program2
-  -- CCDelcontExample.program
-  -- CCDelcontTreeExample.program
-  -- CCDelcontBreadthFirstTraversal.program
-  -- CCDelcontResumableParsing.program
+  Simple.program
+  HigherOrder.program
+  HigherOrder.program2
+  HigherOrder.program3
+  ResetFork.program
+
